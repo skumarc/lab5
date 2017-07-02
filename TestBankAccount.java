0201e478f36e1aed23f8289489adddf2be16b5e1
sanjeev kumar <sanjeev_kumar_chauhan@yahoo.com>
2017-07-02 07:42:41 -0400

    insufficient fund test

--- TestBankAccount.java
+++ TestBankAccount.java
@@ -12,4 +12,11 @@ double amount = account.debit(5);
 Assert.assertEquals(5.0,amount);
 }
 
+@Test
+public void testDebitWithInSufficientFunds(){
+BankAccount account = new BankAccount(10);
+double amount = account.debit(11);
+Assert.assertEquals(10.0,amount);
+}
+
 }
